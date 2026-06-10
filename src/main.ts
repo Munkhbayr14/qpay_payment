4eaaf48bec589382be7ee2b1c8c63bace9cff4d6
Munkhbayar <munkhbayar.m@mongolcontent.mn>
2026-06-10 21:10:30 +0800

    feat(qpay): enable CORS for specified origins and methods

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  app.enableCors({
+    origin: [
+      'https://www.driftub.store',
+      'https://driftub.store',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+  // ───────────────────────────────────────────────────────────────────
+
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
 }
